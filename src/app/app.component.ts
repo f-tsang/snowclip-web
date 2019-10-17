9dd5b26ee887767641871d56b425125e4b3a0f96
Frederick Tsang <frederick.tsang@outlook.com>
2019-10-16 23:55:52 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'clip-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'snowclip';
+}
