6d166e5cbeffc887f4a504f1d85cb549434dc083
Frederick Tsang <frederick.tsang@outlook.com>
2019-11-13 01:40:03 -0800

    Added content to the about page.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core'
+import {Store} from '@ngrx/store'
+import {getPosition, SetBackdropTitle} from 'ft-backdrop'
+import {filter} from 'rxjs/operators'
 
+import {AppBar} from '../app-bar.service'
+
+/**
+ * TODO - Read values from a constants file
+ */
 @Component({
   selector: 'clip-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, OnDestroy {
+  private backdropPositionSub = this.store
+    .select(getPosition)
+    .pipe(filter(position => position === 'down'))
+    .subscribe(() => this.appbar.toggleMenu())
 
-  constructor() { }
-
+  constructor(private appbar: AppBar, private store: Store<any>) {}
   ngOnInit() {
+    this.store.dispatch(new SetBackdropTitle('About'))
   }
-
+  ngOnDestroy() {
+    this.backdropPositionSub.unsubscribe()
+    this.store.dispatch(new SetBackdropTitle(null))
+  }
 }
