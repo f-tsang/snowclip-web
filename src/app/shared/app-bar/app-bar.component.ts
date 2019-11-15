05ef2cd4b7bea070e1992bcff5fb98e3882a9e02
Frederick Tsang <frederick.tsang@outlook.com>
2019-11-15 04:40:53 -0800

    Fixed menu jank by throttling button presses.

--- src/app/shared/app-bar/app-bar.component.ts
+++ src/app/shared/app-bar/app-bar.component.ts
@@ -2,10 +2,12 @@ import {
   AfterViewInit,
   Component,
   Input,
+  OnDestroy,
   ViewChild,
   ViewContainerRef
 } from '@angular/core'
-import {map} from 'rxjs/operators'
+import {Subject} from 'rxjs'
+import {map, throttleTime} from 'rxjs/operators'
 import {AppBar} from 'src/app/app-bar.service'
 
 /**
@@ -43,19 +45,27 @@ import {AppBar} from 'src/app/app-bar.service'
   `,
   styleUrls: ['./app-bar.component.scss']
 })
-export class AppBarComponent implements AfterViewInit {
+export class AppBarComponent implements OnDestroy, AfterViewInit {
   @ViewChild('controls', {read: ViewContainerRef, static: false})
   controlsViewContainerRef: ViewContainerRef
   title$ = this.appbar.title
   icon$ = this.appbar.menuTarget$.pipe(map(fn => fn == null))
 
+  private emitToggleMenu = new Subject<void>()
+  private emitToggleMenuSub = this.emitToggleMenu
+    .pipe(throttleTime(300))
+    .subscribe(() => this.appbar.toggleMenu())
+
   constructor(private appbar: AppBar) {}
   ngAfterViewInit() {
     this.appbar.bindView(this.controlsViewContainerRef)
   }
+  ngOnDestroy() {
+    this.emitToggleMenuSub.unsubscribe()
+  }
 
   toggleMenu() {
-    this.appbar.toggleMenu()
+    this.emitToggleMenu.next()
   }
 
   @Input()
